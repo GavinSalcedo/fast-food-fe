ecf4df518f1ed1f7ded7282e1be834fc034f1f17
Gavi Salcedo <gavi@Gavis-MacBook-Air.local>
2022-10-29 13:54:27 +0800

    Navbar initial commit' ' ;

---
+++ constants/navbar_pages.ts
@@ -0,0 +1,33 @@
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+export const PUBLIC_PAGES = [
+  {
+    key: 'menu',
+    text: 'Menu',
+    path: '/menu'
+  },
+  {
+    key: 'orders',
+    text: 'Orders',
+    path: '/orders'
+  },
+  {
+    key: 'signUp',
+    text: 'Sign Up',
+    path: '/registration'
+  },
+  {
+    key: 'login',
+    text: 'Login',
+    path: '/'
+  }
+];
+
+export const MY_ACCOUNT_LIST = [
+  {
+    key: 'profile',
+    text: 'Profile',
+    path: 'my-profile',
+    icon: AccountCircle
+  }
+];
